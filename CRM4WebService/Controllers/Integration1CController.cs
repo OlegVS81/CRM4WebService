1ab21ebdfc72bffdab2a3c58988c5a4f8c376069
Oleg S <savchukov81@gmail.com>
2019-09-14 14:58:09 +0300

    1

---
+++ CRM4WebService/Controllers/Integration1CController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace CRM4WebService.Controllers
+{
+    public class Integration1CController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/data/forall")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Now server time is: " + DateTime.Now.ToString());
+        }
+
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/data/authenticate")]
+        public IHttpActionResult GetForAuthenticate()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            return Ok("Hello " + identity.Name);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        [Route("api/data/authorize")]
+        public IHttpActionResult GetForAdmin()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var roles = identity.Claims
+                        .Where(c => c.Type == ClaimTypes.Role)
+                        .Select(c => c.Value);
+            return Ok("Hello " + identity.Name + " Role: " + string.Join(",", roles.ToList()));
+        }
+
+        // GET: api/Integration1C/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Integration1C
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Integration1C/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Integration1C/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
